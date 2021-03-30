c7aefcc4326494d944a3fa5a1e26fbc87d42db8e
rubensdemelo <rubensdemelo@gmail.com>
2021-03-30 12:15:13 -0300

    copyright

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,39 @@
+// Copyright (c) 2021 Razeware LLC
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// Notwithstanding the foregoing, you may not use, copy, modify,
+// merge, publish, distribute, sublicense, create a derivative work,
+// and/or sell copies of the Software in any work that is designed,
+// intended, or marketed for pedagogical or instructional purposes
+// related to programming, coding, application development, or
+// information technology. Permission for such use, copying,
+// modification, merger, publication, distribution, sublicensing,
+// creation of derivative works, or sale is expressly withheld.
+//
+// This project and source code may use libraries or frameworks
+// that are released under various Open-Source licenses. Use of
+// those libraries and frameworks are governed by their own
+// individual licenses.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 import 'package:easybooking/booking.dart';
 import 'package:easybooking/detail_page.dart';
 import 'package:easybooking/item_menu.dart';
@@ -18,8 +54,8 @@ class _HomePageState extends State<HomePage> {
   final _timeTextController = TextEditingController();
   final _dayTextController = TextEditingController();
 
-  DateTime _day = DateTime.now();
-  TimeOfDay _time = TimeOfDay.now();
+  DateTime? _day;
+  TimeOfDay? _time;
   double _guests = 1;
   bool _openAir = false;
   bool _special = false;
@@ -63,59 +99,64 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Form(
           key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: ListView(
             physics: BouncingScrollPhysics(),
             children: [
               TextFormField(
                 controller: _nameTextController,
                 decoration: InputDecoration(labelText: 'Name'),
-                autovalidateMode: AutovalidateMode.onUserInteraction,
                 validator: (name) {
                   if (name!.length < 3) {
-                    return "Invalid name";
+                    return 'Invalid name';
                   }
                 },
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp('[a-zA-Z ]')),
+                ],
               ),
               TextFormField(
                 controller: _emailTextController,
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
                 validator: (mail) {
                   if (!mail!.contains('@')) {
-                    return "Invalid e-mail";
+                    return 'Invalid e-mail';
                   }
+                  return null;
                 },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: 'E-mail'),
               ),
               TextFormField(
                 controller: _phoneTextController,
+                validator: (phone) {
+                  if (phone!.isEmpty) {
+                    return 'Invalid phone';
+                  }
+                  return null;
+                },
                 decoration: InputDecoration(labelText: 'Phone'),
                 keyboardType: TextInputType.number,
                 inputFormatters: [
                   FilteringTextInputFormatter.digitsOnly,
                 ],
-                validator: (phone) {
-                  if (phone!.isEmpty) {
-                    return "Invalid phone";
-                  }
-                },
               ),
               Row(
                 children: [
                   Expanded(
-                      child: TextField(
+                      child: TextFormField(
                     controller: _dayTextController,
                     decoration: InputDecoration(labelText: 'Day'),
                     onTap: () async {
-                      final selectDay = await showDatePicker(
+                      final selectedDay = await showDatePicker(
                           context: context,
                           initialDate: DateTime.now(),
                           firstDate: DateTime.now(),
                           lastDate: DateTime.now().add(Duration(days: 7)));
-                      if (selectDay != null) {
-                        _day = selectDay;
+                      if (selectedDay != null) {
+                        _day = selectedDay;
                         _dayTextController.text =
-                            DateFormat.yMd().format(selectDay);
+                            DateFormat.yMd().format(selectedDay);
+                        // _dayTextController.text = _day!.toString();
                       }
                     },
                   )),
@@ -127,7 +168,6 @@ class _HomePageState extends State<HomePage> {
                     onTap: () async {
                       final selectedTime = await showTimePicker(
                           context: context, initialTime: TimeOfDay.now());
-
                       if (selectedTime != null) {
                         _time = selectedTime;
                         _timeTextController.text =
@@ -138,6 +178,8 @@ class _HomePageState extends State<HomePage> {
                           selectedTime.hour,
                           selectedTime.minute,
                         ));
+                        // _timeTextController.text =
+                        //     _time!.format(context).toString();
                       }
                     },
                   )),
@@ -149,26 +191,39 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(width: 16),
                   Expanded(
                     child: Slider(
-                        value: _guests,
-                        min: 1,
-                        max: 8,
-                        onChanged: (value) {
-                          setState(() {
-                            _guests = value;
-                          });
-                        }),
+                      value: _guests,
+                      min: 1,
+                      max: 8,
+                      onChanged: (double guestNumber) {
+                        setState(() {
+                          _guests = guestNumber;
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
               SwitchListTile(
-                  contentPadding: EdgeInsets.all(0),
-                  title: Text('Open air'),
-                  value: _openAir,
-                  onChanged: (value) {
-                    setState(() {
-                      _openAir = value;
-                    });
-                  }),
+                title: Text('Open air'),
+                contentPadding: EdgeInsets.all(0),
+                value: _openAir,
+                onChanged: (bool openAirOption) {
+                  setState(() {
+                    _openAir = openAirOption;
+                  });
+                },
+              ),
+              Row(),
+              CheckboxListTile(
+                title: Text('Special Occasion'),
+                contentPadding: EdgeInsets.all(0),
+                value: _special,
+                onChanged: (bool? specialOccasionOption) {
+                  setState(() {
+                    _special = specialOccasionOption!;
+                  });
+                },
+              ),
               Row(
                 children: _paymentOptions
                     .map((option) => Expanded(
@@ -185,14 +240,6 @@ class _HomePageState extends State<HomePage> {
                         ))
                     .toList(),
               ),
-              CheckboxListTile(
-                  title: Text('Special Occasion'),
-                  value: _special,
-                  onChanged: (value) {
-                    setState(() {
-                      _special = value!;
-                    });
-                  }),
               Divider(),
               Text('Menu'),
               Wrap(
@@ -200,9 +247,9 @@ class _HomePageState extends State<HomePage> {
                 spacing: 8,
                 children: _menuList
                     .map((menuItem) => InputChip(
-                          key: Key(menuItem.label.toString()),
                           label: Text(menuItem.label),
                           selected: menuItem.selected,
+                          showCheckmark: false,
                           onSelected: (selected) {
                             setState(() {
                               menuItem.selected = selected;
@@ -221,9 +268,9 @@ class _HomePageState extends State<HomePage> {
                           _nameTextController.text,
                           _emailTextController.text,
                           _phoneTextController.text,
-                          _day,
-                          _time,
-                          _guests,
+                          _dayTextController.text,
+                          _timeTextController.text,
+                          _guests.ceil().toString(),
                           _openAir,
                           _paymentChoice,
                           _special,
